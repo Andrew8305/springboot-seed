053373c97b31332b50160286faee15314d782aaa
wudi <ustcwudi@126.com>
2018-06-06 14:38:15 +0800

    update SecurityUser Class for Oauth2

--- src/main/java/com/wind/common/SecurityUser.java
+++ src/main/java/com/wind/common/SecurityUser.java
@@ -9,60 +9,32 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.*;
 
 
-@Getter
-@ToString
-public class SecurityUser extends User implements UserDetails {
+@Data
+public class SecurityUser implements UserDetails {
+
+    private Long id;
+    private String username;
+    private String Password;
+    private boolean enabled;
+    private boolean accountNonExpired;
+    private boolean credentialsNonExpired;
+    private boolean accountNonLocked;
+    private Set<GrantedAuthority> authorities;
 
     public SecurityUser(User user) {
         if (user != null) {
             setId(user.getId());
             setUsername(user.getUsername());
-            setAuthType(user.getAuthType());
-            setEmail(user.getEmail());
-            setPhone(user.getPhone());
-            setOpenId(user.getOpenId());
-            setUnionId(user.getUnionId());
-            setSessionKey(user.getSessionKey());
-            setNickname(user.getNickname());
-            setAvatarUrl(user.getAvatarUrl());
-            setAuthority(user.getAuthority());
-            setCity(user.getCity());
-            setProvince(user.getProvince());
-            setCountry(user.getCountry());
-            setGender(user.getGender());
-            setLanguage(user.getLanguage());
-            setRegisterDate(user.getRegisterDate());
+            setPassword(user.getPassword());
             setEnabled(user.getEnabled());
+            setAccountNonExpired(user.getEnabled());
+            setCredentialsNonExpired(user.getEnabled());
+            setAccountNonLocked(user.getEnabled());
+            setAuthorities(new HashSet<>());
+            getAuthorities().add(new SimpleGrantedAuthority(user.getAuthority()));
         } else {
             throw new IllegalArgumentException("Cannot pass null or empty values to constructor");
         }
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return getEnabled();
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        List<GrantedAuthority> result = new ArrayList<>();
-        result.add(new SimpleGrantedAuthority(this.getAuthority()));
-        return result;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return getEnabled();
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return getEnabled();
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return getEnabled();
     }
 
 }
